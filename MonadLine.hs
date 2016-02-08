8585f6cbfd75e069be51a30b9276e45556ec2e15
Sebastian Holzapfel <schnommus@gmail.com>
2016-02-08 14:23:42 +1100

    Changed formatting style to hindent default

--- MonadLine.hs
+++ MonadLine.hs
@@ -7,33 +7,38 @@ import Data.List.Split
 import Data.List
 import Control.Monad
 
-data TerminalColor = TerminalColor { intensity :: ColorIntensity
-                                   , color :: Color }
-                     | Normal
+data TerminalColor
+    = TerminalColor { intensity :: ColorIntensity
+                    , color :: Color}
+    | Normal
 
+data ColorSet = ColorSet
+    { foregroundColor :: TerminalColor
+    , backgroundColor :: TerminalColor
+    }
 
-data ColorSet      = ColorSet      { foregroundColor :: TerminalColor 
-                                   , backgroundColor :: TerminalColor }
-
-data Segment       = Segment       { contents :: IO String
-                                   , terminator :: Char
-                                   , colorSet :: ColorSet
-                                   , bold :: Bool
-                                   , transitionfg :: TerminalColor
-                                   , displayWhen :: IO Bool }
+data Segment = Segment
+    { contents :: IO String
+    , terminator :: Char
+    , colorSet :: ColorSet
+    , bold :: Bool
+    , transitionfg :: TerminalColor
+    , displayWhen :: IO Bool
+    }
 
 -- Helpers for simple segments
-alwaysDisplay :: IO Bool
+alwaysDisplay
+    :: IO Bool
 alwaysDisplay = return True
 
 neverDisplay :: IO Bool
 neverDisplay = return False
 
-
 -- Helpers for bash text printing & colours
 sep = " "
 
 beginNonPrintable = putStr "\\["
+
 endNonPrintable = putStr "\\]"
 
 setSGR_escaped args = do
@@ -45,7 +50,8 @@ resetColors :: IO ()
 resetColors = setSGR_escaped [Reset]
 
 -- Setting colours with support for single colours only
-setColors :: ColorSet -> IO ()
+setColors
+    :: ColorSet -> IO ()
 setColors (ColorSet Normal Normal) = resetColors
 setColors (ColorSet ftcolor Normal) = do
     resetColors
@@ -58,36 +64,41 @@ setColors (ColorSet ftcolor btcolor) = do
     setSGR_escaped [SetColor Background (intensity btcolor) (color btcolor)]
 
 setBold :: Bool -> IO ()
-setBold True = setSGR_escaped[SetConsoleIntensity BoldIntensity]
-setBold False = setSGR_escaped[SetConsoleIntensity NormalIntensity]
+setBold True = setSGR_escaped [SetConsoleIntensity BoldIntensity]
+setBold False = setSGR_escaped [SetConsoleIntensity NormalIntensity]
 
 -- Takes a segment and displays it with regard to the colours of the next
 -- segment's background for smooth blending
-displaySegment :: Segment -> TerminalColor -> IO ()
+displaySegment
+    :: Segment -> TerminalColor -> IO ()
 displaySegment segment nextbg = do
     s <- contents segment
     setBold $ bold segment
     setColors $ colorSet segment
     putStr (sep ++ s ++ sep)
     resetColors
-    setColors $ ColorSet 
-        (case (transitionfg segment) of
-            Normal -> (backgroundColor . colorSet $ segment)
-            _ -> transitionfg segment)
-        nextbg
+    setColors $
+        ColorSet
+            (case (transitionfg segment) of
+                 Normal -> (backgroundColor . colorSet $ segment)
+                 _ -> transitionfg segment)
+            nextbg
     putStr [terminator segment]
     resetColors
 
 -- Actually displays each segment that was previously determined to be
 -- displayable
-doDisplaySegments :: [Segment] -> IO ()
+doDisplaySegments
+    :: [Segment] -> IO ()
 doDisplaySegments [segment] = displaySegment segment Normal
 doDisplaySegments segments = do
-    displaySegment (head segments) (backgroundColor.colorSet.head.tail$segments)
+    displaySegment
+        (head segments)
+        (backgroundColor . colorSet . head . tail $segments)
     doDisplaySegments (tail segments)
 
 -- Filters out any segments that aren't displayable and calss display
 -- function
-displaySegments :: [Segment] -> IO ()
+displaySegments
+    :: [Segment] -> IO ()
 displaySegments segments = filterM displayWhen segments >>= doDisplaySegments
-
--- Segments.hs
+++ Segments.hs
@@ -11,52 +11,77 @@ import Data.List
 import Control.Applicative
 
 -- Data we get from from bash command-line arguments
-getErrorCode = (!!0) <$> getArgs
-getNumJobs   = (!!1) <$> getArgs
+getErrorCode = (!! 0) <$> getArgs
 
-simpleSegment = Segment { contents = return "simpleSegment"
-                        , terminator = right_arrow_hard 
-                        , colorSet = ColorSet { foregroundColor = TerminalColor Vivid White
-                                              , backgroundColor = TerminalColor Dull Blue }
-                        , bold = True
-                        , transitionfg = Normal
-                        , displayWhen = alwaysDisplay }
+getNumJobs = (!! 1) <$> getArgs
 
-userNameSegment = simpleSegment { contents = getEffectiveUserName }
+simpleSegment =    Segment
+    { contents = return "simpleSegment"
+    , terminator = right_arrow_hard
+    , colorSet = ColorSet
+      { foregroundColor = TerminalColor Vivid White
+      , backgroundColor = TerminalColor Dull Blue
+      }
+    , bold = True
+    , transitionfg = Normal
+    , displayWhen = alwaysDisplay
+    }
 
-bashSegment = simpleSegment { contents = return "$" }
+userNameSegment =    simpleSegment
+    { contents = getEffectiveUserName
+    }
 
-directorySegment dir = Segment { contents = return dir
-                               , terminator = right_arrow_soft
-                               , colorSet = ColorSet { foregroundColor = TerminalColor Dull White
-                                                     , backgroundColor = TerminalColor Dull Black }
-                               , bold = False
-                               , transitionfg = TerminalColor Dull White
-                               , displayWhen = alwaysDisplay }
+bashSegment =    simpleSegment
+    { contents = return "$"
+    }
 
-finalDirectorySegment dir = Segment { contents = return dir
-                                    , terminator = right_arrow_hard
-                                    , colorSet = ColorSet { foregroundColor = TerminalColor Vivid White
-                                                          , backgroundColor = TerminalColor Dull Black }
-                                    , bold = True
-                                    , transitionfg = Normal
-                                    , displayWhen = alwaysDisplay }
+directorySegment dir =    Segment
+    { contents = return dir
+    , terminator = right_arrow_soft
+    , colorSet = ColorSet
+      { foregroundColor = TerminalColor Dull White
+      , backgroundColor = TerminalColor Dull Black
+      }
+    , bold = False
+    , transitionfg = TerminalColor Dull White
+    , displayWhen = alwaysDisplay
+    }
 
-errorCodeSegment = Segment { contents = getErrorCode
-                           , terminator = right_arrow_hard 
-                           , colorSet = ColorSet { foregroundColor = TerminalColor Vivid White
-                                                 , backgroundColor = TerminalColor Dull Red }
-                           , bold = True
-                           , transitionfg = Normal
-                           , displayWhen = (/="0") <$> getErrorCode }
+finalDirectorySegment dir =    Segment
+    { contents = return dir
+    , terminator = right_arrow_hard
+    , colorSet = ColorSet
+      { foregroundColor = TerminalColor Vivid White
+      , backgroundColor = TerminalColor Dull Black
+      }
+    , bold = True
+    , transitionfg = Normal
+    , displayWhen = alwaysDisplay
+    }
 
-numJobsSegment = Segment { contents = getNumJobs
-                         , terminator = right_arrow_hard 
-                         , colorSet = ColorSet { foregroundColor = TerminalColor Vivid White
-                                               , backgroundColor = TerminalColor Dull Green }
-                         , bold = True
-                         , transitionfg = Normal
-                         , displayWhen = (/="0") <$> getNumJobs }
+errorCodeSegment =    Segment
+    { contents = getErrorCode
+    , terminator = right_arrow_hard
+    , colorSet = ColorSet
+      { foregroundColor = TerminalColor Vivid White
+      , backgroundColor = TerminalColor Dull Red
+      }
+    , bold = True
+    , transitionfg = Normal
+    , displayWhen = (/= "0") <$> getErrorCode
+    }
+
+numJobsSegment =    Segment
+    { contents = getNumJobs
+    , terminator = right_arrow_hard
+    , colorSet = ColorSet
+      { foregroundColor = TerminalColor Vivid White
+      , backgroundColor = TerminalColor Dull Green
+      }
+    , bold = True
+    , transitionfg = Normal
+    , displayWhen = (/= "0") <$> getNumJobs
+    }
 
 -- Helper used in directorySegments for home directory replacing
 replace old new = intercalate new . splitOn old
@@ -66,10 +91,18 @@ directorySegments = do
     f <- getCurrentDirectory
     h <- getHomeDirectory
     let s = replace h "/~" f
-    let l = map (\d -> if d == "" then "/" else d) (tail $ splitOn "/" s)
-    return $ (map directorySegment (init l)) ++ [finalDirectorySegment $ last l]
+    let l =            map
+                (\d ->
+                      if d == ""
+                          then "/"
+                          else d)
+                (tail $ splitOn "/" s)
+    return $
+        (map directorySegment (init l)) ++ [finalDirectorySegment $ last l]
 
 displayAll :: IO ()
 displayAll = do
     dirSeg <- directorySegments
-    displaySegments $ [userNameSegment] ++ dirSeg ++ [numJobsSegment, errorCodeSegment, bashSegment]
+    displaySegments $
+        [userNameSegment] ++
+        dirSeg ++ [numJobsSegment, errorCodeSegment, bashSegment]
